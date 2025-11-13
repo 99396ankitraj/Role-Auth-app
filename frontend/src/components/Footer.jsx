949468a2656f9c39cfc53b5ad4b1909bb88c232e
Ankit-raj <99396ankitrajput@gmail.com>
2025-11-14 02:30:35 +0530

    changes

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,9 +4,9 @@ export default function Footer() {
       <div className="container" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <div><strong>@2026 Inbotiq Assignment</strong></div>
         <div style={{ display: 'flex', gap: 14 }}>
-          <a href="https://github.com/" target="_blank" rel="noreferrer">GitHub</a>
-          <a href="https://vercel.com/" target="_blank" rel="noreferrer">Vercel</a>
-          <a href="https://render.com/" target="_blank" rel="noreferrer">Render</a>
+          <a href="https://github.com/99396ankitraj" target="_blank" rel="noreferrer">GitHub</a>
+          <a href="https://vercel.com/ankit-rajs-projects-8bd6f294" target="_blank" rel="noreferrer">Vercel</a>
+          <a href="https://dashboard.render.com/" target="_blank" rel="noreferrer">Render</a>
         </div>
       </div>
     </footer>
